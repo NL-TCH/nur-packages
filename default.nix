3e7bb90adfb36dbe9edcb120e76c21263ee5b047
TCH <teunish@outlook.com>
2023-06-21 04:50:10 +0200

    unrevert

--- default.nix
+++ default.nix
@@ -15,6 +15,7 @@
   overlays = import ./overlays; # nixpkgs overlays
 
   example-package = pkgs.callPackage ./pkgs/example-package { };
+  spotify-adblock = pkgs.callPackage ./pkgs/spotify-adblock { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
