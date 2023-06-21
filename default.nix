acbcaf842954c9dfd6a0c2a6e8338584a9a97342
TCH <teunish@outlook.com>
2023-06-21 04:45:41 +0200

    test

--- default.nix
+++ default.nix
@@ -14,9 +14,5 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  spotify-adblock = pkgs.callPackage ./pkgs/spotify-adblock { };
-
-  #example-package = pkgs.callPackage ./pkgs/example-package { };
-  # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
-  # ...
+  spotifyadblock = pkgs.callPackage ./pkgs/spotify-adblock { };
 }
