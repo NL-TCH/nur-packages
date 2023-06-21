358471f4576e78868527a8ea75963aba124d5473
TCH <teunish@outlook.com>
2023-06-21 04:12:00 +0200

    delete test

--- pkgs/testdir/default.nix
+++
@@ -1,8 +0,0 @@
-{ vscode }:
-
-vscode.overrideAttrs (finalAttrs: previousAttrs: {
-  installPhase = previousAttrs.installPhase + ''
-    chmod +x $out/lib/vscode/resources/app/node_modules/node-pty/build/Release/spawn-helper
-    chmod +x $out/lib/vscode/resources/app/node_modules.asar.unpacked/node-pty/build/Release/spawn-helper
-  '';
-})
