af6584f94b7016f49eab9be2c6bed6f095349373
NL-TCH <teunish@outlook.com>
2024-05-03 22:31:42 +0200

    set URL to GITLFS

--- pkgs/ciscoPacketTracer8/default.nix
+++ pkgs/ciscoPacketTracer8/default.nix
@@ -2,7 +2,7 @@
 
 pkgs.ciscoPacketTracer8.overrideAttrs (prev: {
     src = pkgs.fetchurl {
-        url = "https://nc-9064872931098112376.nextcloud-ionos.com/index.php/s/eKymgjnX3MYgFNF/download/CiscoPacketTracer_820_Ubuntu_64bit.deb";
+        url = "https://github.com/NL-TCH/nur-packages/blob/master/pkgs/ciscoPacketTracer8/CiscoPacketTracer822_amd64_signed.deb";
         sha256 = "6cd2b8891df92d2cad8b6fdc47480fc089de085c4f3fe95eb80d5450a2a7f72d";
     };
 })
